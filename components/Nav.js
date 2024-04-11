8b46de42a2ab183e2dae0b7de951fbd48ff1561f
Laura Laqui <lauralaqui@gmail.com>
2024-04-11 17:01:18 -0400

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Nav() {
     return (
         <nav>
-            <div className="nav--icon"><img src="./images/globe-icon.png" alt="nav globe icon" /></div>
+            <div className="nav--icon"><img src="./images/globe.png" alt="nav globe icon" /></div>
             <div className="nav--title">my home journal</div>
         </nav>
     )
